8d099b578dfe84f137795f3d023d39ff183b9046
Harsh-raj <rajharsh209@gmail.com>
2025-01-12 15:58:27 +0530

    Added implementation for Input Embeddings

---
+++ inputEmbedding.py
@@ -0,0 +1,42 @@
+from torch import nn
+import einops
+from vit import Vit
+
+class InputEmbedding(nn.Module):
+  def __init__(self, patch_size = Vit.patch_size, n_channels=Vit.n_channels, device=Vit.device, latent_size=Vit.latent_size, batch_size=Vit.batch_size):
+    super(InputEmbedding, self).__init__()
+    self.latent_size = latent_size
+    self.patch_size = patch_size
+    self.n_channels = n_channels
+    self.device = device
+    self.batch_size = batch_size
+    self.input_size = self.patch_size*self.patch_size*self.n_channels
+    
+    # Linear projection
+    self.linearProjection = nn.Linear(self.input_size, self.latent_size)
+    
+    # Class token
+    self.class_token = nn.Parameter(torch.randn(self.batch_size, 1, self.latent_size)).to(self.device)
+    
+    # Positional embedding
+    self.pos_embedding = nn.Parameter(torch.randn(self.batch_size, 1, self.latent_size)).to(self.device)
+    
+    
+  def forward(self, input_data):
+    input_data = input_data.to(self.device)
+    
+    # Rearrange image into patches
+    patches = einops.rearrange(
+      input_data, 'b c (h h1) (w w1) -> b (h w) (h1 w1 c)', h1=self.patch_size, w1=self.patch_size
+    )
+    
+    linear_projection = self.linearProjection(patches).to(self.device)
+    b, n, _ = linear_projection.shape
+    
+    # Prepend the [class] token to the original linear projection
+    linear_projection = torch.cat((self.class_token, linear_projection), dim=1)
+    pos_embed = einops.repeat(self.pos_embedding, 'b 1 d -> b m d', m=n+1)
+    
+    # Add positional embedding to linear projection
+    linear_projection += pos_embed
+    return linear_projection
