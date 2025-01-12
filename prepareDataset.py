acdc5bef5aabde3c2648122c391d8fb7475bd370
Harsh-raj <rajharsh209@gmail.com>
2025-01-12 16:17:12 +0530

    Added changes considering filesystem for google colab is different

--- prepareDataset.py
+++ prepareDataset.py
@@ -7,7 +7,7 @@ from vit import Vit
 class PrepareDataset:
   transform_training_data = Compose([RandomCrop(32, padding=4), Resize((224)), RandomHorizontalFlip(), ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-  train_data = torchvision.datasets.CIFAR10(root='/home/achalhoub/Desktop/ViT_CIFAR10_data', train=True, download=False, transform=transform_training_data)
+  train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_training_data)
 
   trainloader = torch.utils.data.DataLoader(train_data, batch_size=Vit.batch_size,shuffle=True, num_workers=2)
 
